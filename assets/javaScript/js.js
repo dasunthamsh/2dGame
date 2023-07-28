04b5f694ef0d38aad93a0506b15b1dd84b459425
dasunthamsh <tunathamash@gmail.com>
2023-07-28 21:45:19 +0530

    character run animation complete

--- assets/javaScript/js.js
+++ assets/javaScript/js.js
@@ -20,12 +20,61 @@ function idleAnimationStart(){
     idleAnimationNumber = setInterval(idleAnimation,200);
 }
 
-//character run animation
 
+//character run animation-------------------------------------------------
+
+
+var runImageNumber=0;
+var runAniamtionNumber =0;
     function runAnimation(){
 
-        boy.src =
+        runImageNumber=runImageNumber+1;
+
+        if(runImageNumber==11){
+            runImageNumber = 1;
+        }
+
+        boy.src = "assets/img/png/Run("+ runImageNumber +").png";
     }
+
+    function runAnimationStart(){
+       runAniamtionNumber = setInterval(runAnimation,100);
+       clearInterval(idleAnimationNumber);  // Stop idle animation
+    }
+
+    //when press start boy start to running ------------------------------------
+
+ function keyCheck(event){
+    // alert(event.keyCode);
+    // console.log(event);
+     var keyCode = event.keyCode;
+
+     if(keyCode==13){
+         if(runAniamtionNumber==0){
+             runAnimationStart();
+         }
+
+     }
+
+     if (moveBackgroundAnimationId==0){
+         moveBackgroundAnimationId = setInterval(moveBackground,100);
+     }
+ }
+
+ //move background when boy runs------------------------------------
+
+
+var backgraoundImagePositionX=0;
+    moveBackgroundAnimationId=0;
+
+    function moveBackground(){
+
+        backgraoundImagePositionX = backgraoundImagePositionX -20;
+
+        document.getElementById("background").style.backgroundPositionX = backgraoundImagePositionX + "px";
+
+    }
+
 
 
 
