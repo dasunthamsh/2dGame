f601fd30a40c72ccc8486d827973ab76f93781e4
dasunthamsh <tunathamash@gmail.com>
2023-07-28 17:03:47 +0530

    some images added

---
+++ assets/javaScript/js.js
@@ -0,0 +1,23 @@
+var boy =  document.getElementById("boy");
+idleImageNumber =0;
+idleAnimationNumber=0;
+
+    function  idleAnimation(){
+
+        idleImageNumber == idleImageNumber+1
+
+        if(idleImageNumber==11){
+            idleImageNumber =1;
+    }
+
+    boy.src = "assets/img/png/idle("+idleImageNumber+").png";
+}
+
+function idleAnimationStart(){
+    idleAnimationNumber = setInterval(idleAnimation,1000);
+}
+
+
+
+
+
